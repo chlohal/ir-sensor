f857b148323d4f1063286c54cbf4b2fd01dfd38e
chloe h <coleh@coleh.net>
2023-06-09 00:28:43 -0400

    error which wasn't caught by cargo check?

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,10 @@ pub use crate::button::Button;
 
 //Debug implementation for non-raspi
 #[cfg( not(target_arch = "arm"))]
-pub fn button_events() -> Result<Receiver<Button>, Box<dyn Error>> {
-    let (tx, rx) = mpsc::channel();
+pub fn button_events() -> Result<std::sync::mpsc::Receiver<Button>, Box<dyn Error>> {
+    let (tx, rx) = std::sync::mpsc::channel();
     
-    thread::spawn(move || {
+    std::thread::spawn(move || {
         loop {
 
         }
