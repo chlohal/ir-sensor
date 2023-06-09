5cff57fba688375bdd7060a0125442f77eca2029
Chloe H <email@clh.sh>
2023-06-09 01:20:28 +0000

    Some work on button enum

---
+++ src/button.rs
@@ -0,0 +1,48 @@
+pub enum Button {
+    //Main face of remote
+    Home,
+    Menu,
+    ArrowUp,
+    ArrowLeft,
+    ArrowRight,
+    ArrowDown,
+    Ok,
+    Exit,
+    Hamburger,
+    Play,
+    Pause,
+    Rewind,
+    FastForward,
+    Record,
+    Stop,
+    Yellow,
+    Blue,
+    Red,
+    Green,
+    Num1,
+    Num2,
+    Num3,
+    Num4,
+    Num5,
+    Num6,
+    Num7,
+    Num8,
+    Num9,
+    Num0,
+    NumEnter,
+    NumMinus
+
+    //Left side
+    Input,
+    ChannelDown,
+    ChannelUp,
+    ChannelPrevious,
+    Eject
+
+    //Right side
+    Power,
+    VolumeUp,
+    VolumeDown,
+    VolumeMute,
+
+}
